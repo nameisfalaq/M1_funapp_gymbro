241cff26b74138c9d53e0b89d05fa931589fed05
nameisfalaq <63052065+nameisfalaq@users.noreply.github.com>
2021-11-25 19:48:33 +0530

    Uploaded the program file

---
+++ calories_calculator.c
@@ -0,0 +1,106 @@
+# include <stdio.h>
+#include <string.h>
+
+char gender;
+float weight;
+float height;
+float age;
+int activity_level;
+
+float calories(float h, float w, float a);
+int main(){
+    char user_name[50];
+
+    printf("Hi this is your Gymbro \nYour calorie calculator who tells you about how many calories your body needs.");
+    printf("\nMay I please know your name...");
+    gets(user_name);
+    printf("\nHi %s. Let's Get started.", user_name);
+    printf("To get to know about your Basal Metabolic Rate we'll need you to specify your gender so that we can provide you accurate info.\n");
+    printf("Type M if you are Male and type F if you're a Female.\n");
+    scanf("%c",&gender);
+
+    if (gender == 'M'){
+        printf("Sheesh!! Let's go\n");
+    }
+    else{
+        printf("Sheesh!!! Let's go\n");
+    }
+
+    printf("Enter your weight in pounds(lbs):: 1kg==2.204 lbs\n");
+    scanf("%f", &weight);
+
+    printf("Enter your height in inches(in)):: 1ft==12 inches\n");
+    scanf("%f", &height);
+
+    printf("Enter your age\n");
+    scanf("%f", &age);
+
+    printf("State you activity level and how active are you on a daily basis:\n");
+    printf("Type 1 if you do sedentary movement activities.\n");
+    printf("Type 2 if you do light workout 1-2x a week.\n");
+    printf("Type 3 if you do moderate workout 3-5x a week.\n");
+    printf("Type 4 if you do extreme workout 7x a week.\n");
+    scanf("%d", &activity_level);
+
+    printf("%f", calories(height,weight,age));
+
+    return 0;
+}
+
+float calories(float h, float w, float a){
+    float woman_weight;
+    float woman_height;
+    float woman_age;
+    float woman_bmr;//bmr=basal metaboic rate
+    float man_weight;
+    float man_height;
+    float man_age;
+    float man_bmr;
+    float final_bmr;
+    switch(gender){
+case 'F':
+    woman_weight=weight * 4.35;
+    woman_height=height * 4.7;
+    woman_age= age *4.7;
+    woman_bmr=woman_weight+woman_height-woman_age+655;
+
+    if (2==activity_level){
+        final_bmr=woman_bmr*1.375;
+    }
+    else if(1==activity_level){
+        final_bmr=woman_bmr*1.2;
+    }
+    else if(3==activity_level){
+        final_bmr=woman_bmr*1.55;
+    }
+    else {
+        final_bmr=woman_bmr*1.725;
+    }
+    printf("Your BMR before taking activity level in consideration is %f\n",woman_bmr);
+    printf("Your actual BMR is %f",final_bmr);
+    break;
+case 'M':
+    man_weight=weight * 6.23;
+    man_height=height * 12.7;
+    man_age= age *6.8;
+    man_bmr=man_weight+man_height-man_age+66;
+
+    if (2==activity_level){
+        final_bmr=man_bmr*1.375;
+    }
+    else if(1==activity_level){
+        final_bmr=man_bmr*1.2;
+    }
+    else if(3==activity_level){
+        final_bmr=man_bmr*1.55;
+    }
+    else {
+        final_bmr=man_bmr*1.725;
+    }
+    printf("Your BMR before taking activity level in consideration is %f\n",man_bmr);
+    printf("Your actual BMR is %f",final_bmr);
+    break;
+    }
+    return final_bmr;
+}
+
